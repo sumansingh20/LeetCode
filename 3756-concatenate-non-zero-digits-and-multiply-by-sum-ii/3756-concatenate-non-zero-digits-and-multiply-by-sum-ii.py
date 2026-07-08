ce18c0fadca8a15acc29ac289cdcdf90d81ff7d8
suman kumar <146518730+sumansingh20@users.noreply.github.com>
2026-07-08 11:02:30 +0530

    Time: 1034 ms (6.17%), Space: 56.4 MB (54.32%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,41 +1,62 @@
-import bisect
-
 class Solution:
-    def sumAndMultiply(self, s: str, queries: list[list[int]]) -> list[int]:
+    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
         mod = 1000000007
-        
+
         pos = []
-        val = []
+        nums = []
+
         for i in range(len(s)):
-            if s[i] != '0':
+            if s[i] != "0":
                 pos.append(i)
-                val.append(int(s[i]))
-                
-        k = len(val)
-        
-        p_sum = [0] * (k + 1)
-        p_val = [0] * (k + 1)
-        p_10 = [1] * (k + 1)
-        
-        for i in range(k):
-            p_sum[i + 1] = p_sum[i] + val[i]
-            p_val[i + 1] = (p_val[i] * 10 + val[i]) % mod
-            p_10[i + 1] = (p_10[i] * 10) % mod
-            
+                nums.append(int(s[i]))
+
+        n = len(nums)
+
+        preSum = [0] * (n + 1)
+        preNum = [0] * (n + 1)
+        power = [1] * (n + 1)
+
+        for i in range(n):
+            preSum[i + 1] = preSum[i] + nums[i]
+            preNum[i + 1] = (preNum[i] * 10 + nums[i]) % mod
+            power[i + 1] = (power[i] * 10) % mod
+
         ans = []
-        
+
         for l, r in queries:
-            left_idx = bisect.bisect_left(pos, l)
-            right_idx = bisect.bisect_right(pos, r) - 1
-            
-            if left_idx <= right_idx:
-                length = right_idx - left_idx + 1
-                
-                num = (p_val[right_idx + 1] - p_val[left_idx] * p_10[length]) % mod
-                total = p_sum[right_idx + 1] - p_sum[left_idx]
-                
-                ans.append((num * total) % mod)
-            else:
+
+            left = 0
+            right = n - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+                if pos[mid] >= l:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            start = left
+
+            left = 0
+            right = n - 1
+
+            while left <= right:
+                mid = (left + right) // 2
+                if pos[mid] <= r:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+            end = right
+
+            if start > end:
                 ans.append(0)
-                
+                continue
+
+            length = end - start + 1
+            number = (preNum[end + 1] - preNum[start] * power[length]) % mod
+            total = preSum[end + 1] - preSum[start]
+
+            ans.append((number * total) % mod)
+
         return ans
