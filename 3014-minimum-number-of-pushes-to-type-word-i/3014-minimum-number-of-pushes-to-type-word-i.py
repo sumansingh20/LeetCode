544d86312d28614b65f571fa631c018fabb15fc1
suman kumar <146518730+sumansingh20@users.noreply.github.com>
2026-07-30 22:56:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (83.33%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n = len(word)
+        ans = 0
+        for i in range(n):
+            ans += (i // 8) + 1
+        return ans
